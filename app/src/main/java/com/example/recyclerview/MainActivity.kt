b619e7ef66ec17fddbb529f96692ee7e1baee6db
Devin AF <devinayersfunata@gmail.com>
2025-11-04 20:27:59 +0700

    nomer 65 - 68

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.recyclerview
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,10 +16,15 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var _nama : Array<String>
-    private lateinit var _karakter : Array<String>
-    private lateinit var _deskripsi : Array<String>
-    private lateinit var _gambar : Array<String>
+//    private lateinit var _nama : Array<String>
+//    private lateinit var _karakter : Array<String>
+//    private lateinit var _deskripsi : Array<String>
+//    private lateinit var _gambar : Array<String>
+
+    private lateinit var _nama : MutableList<String>
+    private lateinit var _karakter : MutableList<String>
+    private lateinit var _deskripsi : MutableList<String>
+    private lateinit var _gambar : MutableList<String>
 
     private var arWayang = arrayListOf<dcWayang>()
     private lateinit var _rvWayang: RecyclerView
@@ -38,11 +45,18 @@ class MainActivity : AppCompatActivity() {
         TampilanData()
     }
 
+//    private fun SiapkanData() {
+//        _nama = resources.getStringArray(R.array.namaWayang)
+//        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+//        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+//        _gambar = resources.getStringArray(R.array.gambarWayang)
+//    }
+
     private fun SiapkanData() {
-        _nama = resources.getStringArray(R.array.namaWayang)
-        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
-        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
-        _gambar = resources.getStringArray(R.array.gambarWayang)
+        _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
+        _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
     }
 
     fun TambahData() {
@@ -85,8 +99,51 @@ fun TampilanData() {
             intent.putExtra("kirimData", data)
             startActivity(intent)
         }
+
+        override fun delData(pos: Int) {
+            AlertDialog.Builder(this@MainActivity)
+            .setTitle("HAPUS DATA")
+                .setMessage("Apakah Benar Data \"+_nama[pos]+\" akan dihapus ?")
+                .setPositiveButton(
+                    "HAPUS",
+                    DialogInterface.OnClickListener { dialog, which ->
+                _gambar.removeAt(pos)
+                _nama.removeAt(pos)
+                _deskripsi.removeAt(pos)
+                _karakter.removeAt(pos)
+                TambahData()
+                TampilanData()
+            }
+            )
+            .setNegativeButton(
+                "BATAL",
+            DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(
+                    this@MainActivity,
+                "Data Batal Dihapus",
+                Toast.LENGTH_LONG
+                ).show()
+            }
+            )
+            .show()
+        }
+
+        fun TambahData() {
+            arWayang.clear()
+            for (position : Int in _nama.indices) {
+                val data = dcWayang(
+                    _gambar[position],
+                    _nama[position],
+                    _karakter[position],
+                    _deskripsi[position]
+                )
+                arWayang.add(data)
+            }
+        }
+
     })
 
 }
+
 
 }
--- app/src/main/java/com/example/recyclerview/adapterRecView.kt
+++ app/src/main/java/com/example/recyclerview/adapterRecView.kt
@@ -64,4 +64,6 @@ class adapterRecView(private val listWayang: List<dcWayang>) :
             onItemClickCallback.delData(position)
         }
     }
+
+
 }
