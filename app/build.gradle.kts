5db801acb5b145883a32a663b5fea4745feeff12
Devin AF <devinayersfunata@gmail.com>
2025-11-11 18:04:46 +0700

    nomer 5 dan 6 SharedPreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.code.gson:gson:2.13.2")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
